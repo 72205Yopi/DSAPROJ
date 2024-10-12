c27fbe4583921430d8ec93fd87f7806457f0b697
USER <USER@LAPTOP-R57P3NKQ>
2024-10-12 11:58:51 +0800

    20 words added for eng to ilocano

--- DSADICTIONARY/src/Dictionary/Book1.java
+++ DSADICTIONARY/src/Dictionary/Book1.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+//      Eng  to ILocano 
 package Dictionary;
-
-/**
- *
- * @author USER
- */
+import java.util.HashMap;
 public class Book1 {
-    
+    public static void main(String[] args) {
+        HashMap<String, String> EngToIlo = new HashMap<>();
+        EngToIlo.put("abandon","baybay-an, ibati, licudan"); //1
+        EngToIlo.put("abdomen","tian");  //2
+        EngToIlo.put("abduct","itaray, agawen"); //3
+        EngToIlo.put("abeyance","panagur-uray, panangteng-ngel");//4
+        EngToIlo.put("abide","agtaeng");//4
+        EngToIlo.put("ability","Cabaelan");//5
+        EngToIlo.put("able","napigsa, addan cabaelan, addaan bileg");//6
+        EngToIlo.put("abnormal","naisupadi iti caaduan");//7
+        EngToIlo.put("aboard","nacalugan, nacasacay");//8
+        EngToIlo.put("abolish","iccaten, pucawen");//9
+        EngToIlo.put("abortive","naganac a saanna a tiempo");//10
+        EngToIlo.put("about","maipanggep, maipapan");//11
+        EngToIlo.put("above","ñgato, ngatuen");//12
+        EngToIlo.put("abridge","yababa, gupongen");//13
+        EngToIlo.put("abrupt","dagus la nga, kellaat");//14
+        EngToIlo.put("absent","awan, saan a simbrec");//15
+        EngToIlo.put("absolute","macabalin, ti isuamin");//16
+        EngToIlo.put("absorb","sisipen, maicuyog, agsepen");//17
+        EngToIlo.put("absurd","nacacat-catawa, awan banagna, di rumbeng");//18
+        EngToIlo.put("abundance","kinaadu");//19
+        EngToIlo.put("abuse","panagsalungasibng");//20
+    }
+     
 }
