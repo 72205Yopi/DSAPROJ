c4de1556cf96fef6d57adf089632a7443df19ac1
USER <USER@LAPTOP-R57P3NKQ>
2024-10-13 21:56:06 +0800

    added book2

---
+++ DSADICTIONARY/src/Dictionary/Book2.java
@@ -0,0 +1,146 @@
+package Dictionary;
+import java.util.HashMap;
+public class Book2 {
+
+    public static void main(String[] args) {
+       HashMap<String, String> TagToIlo = new HashMap<>();
+         
+        //letter A in Tagalog to Ilocano
+        TagToIlo.put("pinabayaan","baybay-an,"); //1
+        TagToIlo.put("tiyan","tian");  //2
+        TagToIlo.put("dukutin","itaray"); //3
+        TagToIlo.put("pagpapaliban","panagur-uray");//4
+        TagToIlo.put("kasaganaan","kinaadu");//5
+        TagToIlo.put("pang-abuso","panagsalungasibng");//6
+        //letter B in Tagalog to Ilocano
+        TagToIlo.put("bata","ubing, tagibi");//7
+        TagToIlo.put("likod","licud, bucot");//8
+        TagToIlo.put("paurong","palicud, pasanud");//9
+        TagToIlo.put("bekon","tosino");//10
+        TagToIlo .put("masama","dakes");//11
+        TagToIlo.put("tsapa","medalia");//12
+        //letter C in Tagalog to Ilocano 
+        TagToIlo.put("taksi","auto");//13
+        TagToIlo.put("kabaret","cabaret");//14
+        TagToIlo.put("repolyo","repollo");//15
+        TagToIlo.put("keik","bibingca");//16
+        TagToIlo.put("tawag","ayab");//17
+        TagToIlo.put("kalmado","natalna");//18
+        //letter D in Tagalog to Ilocano
+        TagToIlo.put("araw-araw","inaldaw");//19
+        TagToIlo.put("maselan","bassit");//20
+        TagToIlo.put("gatasan"," paggatasan");//21
+        TagToIlo.put("prinsa","tambac");//22
+        TagToIlo.put("pinsala","pucaw");//23
+        TagToIlo.put("dalaga","balasang");//24
+        TagToIlo.put("sayaw","sumasala");//25        
+        //Letter E in Tagalog to Ilocano
+        TagToIlo.put("bawat isa","tunggal maysa");//26
+        TagToIlo.put("sabik","mailiw");//27
+        TagToIlo.put("tainga","lapayag");//28
+        TagToIlo.put("maaga","nasapa");//29
+        TagToIlo.put("markahan","marcaan");//30
+        TagToIlo.put("taimtim","napasnec");//31        
+        //letter F in Tagalog to Ilocano
+        TagToIlo.put("tela","lupot");//32
+        TagToIlo.put("katha","panagaramid");//33
+        TagToIlo.put("muka"," sanguen");//34
+        TagToIlo.put("pasilidad","kinalaca");//35
+        TagToIlo.put("paksimile","copia");//36
+        TagToIlo.put("pakultad","mamaestro");//37
+        //Letter G in Tagalog to Ilocano    
+        TagToIlo.put("makakuha","gunggonna");//38
+        TagToIlo.put("malakas na hangin","allawig");//39
+        TagToIlo.put("apdo","pait");//40
+        TagToIlo.put("galante","natacneng");//41
+        TagToIlo.put("galerya","pagbuyaan");//42
+        TagToIlo.put("galon","galon");//43        
+        //Letter H in Tagalog to Ilocano
+        TagToIlo.put("ugali","uglai");//44
+        TagToIlo.put("pasilyo","salas");//45
+        TagToIlo.put("huminto","agsardeng");//46
+        TagToIlo.put("hamon","hamon");//47
+        TagToIlo.put("martilyo","martillo");//48
+        TagToIlo.put("duyan","indayon");//49
+        TagToIlo.put("kamay","ima");//50
+        
+         //15words
+         
+         
+      //Letter I-M in Tagalog to Ilocano
+      TagToIlo.put("yelo","hielo");//51
+      TagToIlo.put("surbetes","sorbetes");//52
+      TagToIlo.put("ideya","panunot");//53
+      //letter J in Tagalog to Ilocano
+      TagToIlo.put("papok","danogen");//54
+      TagToIlo.put("kulungan","pagbaludan");//55
+      TagToIlo.put("panga","timid");//56
+      //letter K in Tagalog to Ilocano
+      TagToIlo.put("susi","tulbec");//57
+      TagToIlo.put("sipa","cugtar");//58
+      TagToIlo.put("mabait","clase");//59
+      //letter L in Tagalog to Ilocano
+      TagToIlo.put("karatula","marcaan");//60
+      TagToIlo.put("paggawa"," aramid");//61
+      TagToIlo.put("puntas","laso");//62
+      //letter M in Tagalog to Ilocano
+      TagToIlo.put("galit","macapungtot");//63
+      TagToIlo.put("mail","correo");//64
+      TagToIlo.put("marilag","dackel");//65
+      
+      
+      
+      //N-S 20words in Tagalog to Ilocano
+      //letter n 3 words in Tagalog to Ilocano
+      TagToIlo.put("kuko","lansa");//66
+      TagToIlo.put("hubad","labus");//67
+      TagToIlo.put("pangalan","nagan");//68
+      //letter o 3  words in Tagalog to Ilocano
+      TagToIlo.put("oras","ti horas");//67
+      TagToIlo.put("sumunod","agtungpal");//68
+      TagToIlo.put("bagay","sumupiat");//69
+      //letter p 3 words in Tagalog to Ilocano
+      TagToIlo.put("bilis","addang");//70
+      TagToIlo.put("kadena","candado");//71
+      TagToIlo.put("sakit","utot");//72
+       //letter q 3 words in Tagalog to Ilocano
+       TagToIlo.put("pugo","pugo");//73
+       TagToIlo.put("kalidad","clase");//74
+       TagToIlo.put("awayan","apa");//75
+       //letter r 4 words in Tagalog to Ilocano
+       TagToIlo.put("ulan","tudo");//76
+       TagToIlo.put("rainbow","bullalayaw");//77
+       TagToIlo.put("bahaghari","matutudo");//78
+       TagToIlo.put("bihira","manmano");//79
+      //letter s 4 words in Tagalog to Ilocano
+      TagToIlo.put("malungko","naliday");//80
+      TagToIlo.put("ligtas","natalged");//81
+      TagToIlo.put("asin","asin");//82
+      TagToIlo.put("maalat","naapgad");//83
+      //T-Z: 10 words in Tagalog to Ilocano
+      //letter t in Tagalog to Ilocano
+      TagToIlo.put("lamesa","dalang");//84
+      TagToIlo.put("tableta","tail");//85
+      TagToIlo.put("tablet","tail");//86
+      //letter u in Tagalog to Ilocano
+      TagToIlo.put("pangit","naalas");//87
+      TagToIlo.put("tiyo","uliteg");//88
+      TagToIlo.put("sa ilalim","siroc");//89
+      //letter v in Tagalog to Ilocano
+      TagToIlo.put("bakante","awang");//90
+      TagToIlo.put("wasto","mabalin");//91
+      TagToIlo.put("halaga","pateg");//92
+      //letter w in Tagalog to Ilocano
+      TagToIlo.put("baiwang","sibet");//93
+      TagToIlo.put("maghintay","aguray");//94
+      TagToIlo.put("pader","diding");//95
+      //letter y in Tagalog to Ilocano
+      TagToIlo.put("nami","ubi");//96
+      TagToIlo.put("taon","tawen");//97
+      TagToIlo.put("dilaw","duyaw");//98
+      //letter z in Tagalog to Ilocano
+      TagToIlo.put("sero","awan");//99
+      TagToIlo.put("sona","paset");//100
+        
+    }
+}
